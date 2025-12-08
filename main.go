4c2b8089736019e72e43169e6106fef7d27ab40a
arglp <leopold.prueller@gmx.at>
2025-12-08 20:32:58 +0100

    new stuff

--- main.go
+++ main.go
@@ -1,3 +1,62 @@
 package main
 
-func main()
+import (
+	"net/http"
+	"log"
+	"sync/atomic"
+	"fmt"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK\n"))
+}
+
+func main () {
+	var apiCfg apiConfig
+	apiCfg.fileserverHits.Store(0)
+
+	sMux := http.NewServeMux()
+	s := http.Server{}
+	s.Addr = ":8080"
+	s.Handler = sMux
+	fileServer := http.FileServer(http.Dir("."))
+	
+	sMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+	sMux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK\n"))
+	})
+	sMux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	sMux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Fatal("fatal error:", err)
+	}
+}
